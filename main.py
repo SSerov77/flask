a6ab3d0c64c37c5f35fcd100324af6213d01544e
Я <sergo.sergej.05@yandex.ru>
2022-05-10 16:39:33 +0500

    Реклама с картинкой

--- main.py
+++ main.py
@@ -14,9 +14,46 @@ def image_mars():
                     </head>
                     <body>
                         <h1>Жди нас, Марс!</h1>
-                        <img src="{url_for('static', filename='image/mars.png')}">
+                        <img src="{url_for('static', filename='img/mars.png')}">
                         <p>Вот она какая, красная планета.</p>
                     </body>
+                </html>"""
+
+
+# Реклама с картинкой
+@app.route('/promotion_image')
+def promotion_image():
+    return f"""<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
+                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                    <title>Колонизация</title>
+                  </head>
+                  <body>
+                    <h1>Жди нас, Марс!</h1>
+                    <img src="{url_for('static', filename='img/mars.png')}">
+                    <div class="alert alert-danger" role="alert">
+                      Человечество вырастает из детства.
+                    </div>
+                    <div class="alert alert-warning" role="alert">
+                      Человечеству мала одна планета.
+                    </div>
+                    <div class="alert alert-success" role="alert">
+                      Мы сделаем обитаемыми безжизненные пока планеты.
+                    </div>
+                    <div class="alert alert-info" role="alert">
+                      И начнем с Марса!
+                    </div>
+                    <div class="alert alert-primary" role="alert">
+                      Присоединяйся!
+                    </div>
+                  </body>
                 </html>"""
 
 
